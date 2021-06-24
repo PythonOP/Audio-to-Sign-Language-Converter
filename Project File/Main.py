f83104ca42e0b9fdc54b6f51738178a4d8ec7e19
Soumyajit Saha <soumyajitsaha33@gmail.com>
2021-06-24 11:20:20 +0530

    Arranged the pictures into a directory

--- Project File/Main.py
+++ Project File/Main.py
@@ -10,7 +10,7 @@ img = list()
 def conversionAction():
     for widgets in f2.winfo_children():
         widgets.destroy()
-    global imgs
+    global img
     text = t1.get()
     i = 1
     z = 0
