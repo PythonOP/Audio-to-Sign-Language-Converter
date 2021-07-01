5dbba3563150f34b916c37d6a072a2bbede30f6d
Soumyajit Saha <soumyajitsaha33@gmail.com>
2021-07-01 12:18:38 +0530

    ConversionAction.py is optimised

--- Project File/ConversionAction.py
+++ Project File/ConversionAction.py
@@ -8,41 +8,22 @@ def conversionaction(text, signdisplayerframe):
     for Signwidgets in signdisplayerframe.winfo_children():
         Signwidgets.destroy()
     global img
-    i = 0
-    j = 0
-
-    z = 0
-    # 21 blocks
+    i = j = z = 0
+    # 22 blocks
     words = text.split()
     for x in words:
-        if len(x) <= (21 - i):
-            for alphabet in x:
-                img.append(alphabet)
-                img[z] = Image.open("../Project File/Sign_Pictures/" + alphabet + ".jpg")
-                img[z] = img[z].resize((70, 70), Image.ANTIALIAS)
-                img[z] = ImageTk.PhotoImage(img[z])
-                signalphabet = Label(signdisplayerframe, image=img[z])
-                signalphabet.grid(row=j, column=i, pady=10)
-                z += 1
-                i += 1
-            signalphabet = Label(signdisplayerframe)
-            signalphabet.grid(row=j, column=i, padx=10)
-            i += 1
-        else:
+        if not len(x) <= (22 - i):
             i = 0
             j += 1
-            for alphabet in x:
-                img.append(alphabet)
-                img[z] = Image.open("../Project File/Sign_Pictures/" + alphabet + ".jpg")
-                img[z] = img[z].resize((70, 70), Image.ANTIALIAS)
-                img[z] = ImageTk.PhotoImage(img[z])
-                signalphabet = Label(signdisplayerframe, image=img[z])
-                signalphabet.grid(row=j, column=i, pady=10)
-                z += 1
-                i += 1
-            signalphabet = Label(signdisplayerframe)
-            signalphabet.grid(row=j, column=i, padx=10)
+        for alphabet in x:
+            img.append(alphabet)
+            img[z] = Image.open("../Project File/Sign_Pictures/" + alphabet + ".jpg")
+            img[z] = img[z].resize((65, 65), Image.ANTIALIAS)
+            img[z] = ImageTk.PhotoImage(img[z])
+            signalphabet = Label(signdisplayerframe, image=img[z])
+            signalphabet.grid(row=j, column=i, pady=10)
+            z += 1
             i += 1
-
-
-
+        signalphabet = Label(signdisplayerframe, bg='#1c1c1c', fg='#1c1c1c')
+        signalphabet.grid(row=j, column=i)
+        i += 1
